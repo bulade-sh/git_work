7be05e0bcb686eaa5ba60949cad2efd30eeecc4a
bulade-shaf <bulatshafikov05@gmail.com>
2024-11-17 17:07:28 +0500

    Комментирование строк кода, где добавляю строки в вектор

--- main.cpp
+++ main.cpp
@@ -12,12 +12,13 @@ void reading(const string &fileName, vector<string> &strings)
 // так как мы вручную добавляем в вектор строки,
 // то нужно, чтобы вектор strings был не const.
 // То есть при проверке мы его убираем.
-void out(vector<string> &strings)
+void out(const vector<string> &strings)
 {
+    /*
     strings.push_back("name - Bulat");
     strings.push_back("surname - Shafikov");
     strings.push_back("age - 19");
-
+    */
     for (const string &line : strings) {
         cout << line << endl;
     }
